1eb4dd1324273b120d87bfe77bf92bd9bfb5ac42
Marcio <marciogomes@alu.ufc.br>
2023-04-12 23:25:35 -0300

    validate and errors - first part

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+import "express-async-errors";
 import express from "express";
 import { routes } from "./routes";
+import { errorMiddleware } from "./errors/errorMiddleware";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(errorMiddleware);
 
 /* const routes = Router(); */
 
